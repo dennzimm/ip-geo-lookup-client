9cef2b0adef401dacfd15c7b173c001b545b371d
dennzimm <dennzimm@icloud.com>
2024-06-22 21:02:53 +0200

    feat: add tanstack query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { queryClient } from "@/config/query-client";
 import { NextUIProvider } from "@nextui-org/system";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { useRouter } from "next/navigation";
@@ -15,10 +18,17 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <NextThemesProvider attribute="class" defaultTheme="dark" {...themeProps}>
-        {children}
-      </NextThemesProvider>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider navigate={router.push}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="dark"
+          {...themeProps}
+        >
+          {children}
+        </NextThemesProvider>
+      </NextUIProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
